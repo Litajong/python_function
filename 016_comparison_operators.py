ccb996c9ab9c5a4127db5d9b0fcb542640eed2de
Litajong <58197187+Litajong@users.noreply.github.com>
2020-03-28 18:37:50 -0500

    Add files via upload

---
+++ 016_comparison_operators.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Comparison Operators (>, <, >=, <=, ==, Assign = vs compare ==, !=)
+# comparison operatiors are boolean and return True or False
+
+3 > 5
+
+
+# In[3]:
+
+
+3 >= 3   
+
+
+# In[4]:
+
+
+# Assign vs Compare
+# assign points equal to 3
+point = 0
+
+# test if points is equal to
+point == 9
+
+
+# In[5]:
+
+
+point < 9
+
+
+# In[7]:
+
+
+# Not equal !=
+x = 3
+print("x not equal 9 is", x != 9)
+print("x equal 3 is", x == 3)
+
+
+# In[8]:
+
+
+# Task: comparison operators
+
+x = 9 + 4
+
+# [ ] create a test to print() True or False for x is equal to 13
+print("x is equal to 13 is", x == 13)
+
+
+# In[9]:
+
+
+# [ ] create a test to print True or False for 3 + 3 is greater than 2 + 4
+print("3 + 3 is greater than 2 + 4 is", 3 + 3 == 2 + 4)
+
+
+# In[ ]:
+
+
+
+
